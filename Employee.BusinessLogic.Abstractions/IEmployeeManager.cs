a9a7cbcfe7c732991f2b52c36b4524aa99ec5ca7
thomasgalliker <thomas@bluewin.ch>
2015-11-29 21:40:04 +0100

    Changed IEnumerable to ICollection because we expect materialized data

--- Employee.BusinessLogic.Abstractions/IEmployeeManager.cs
+++ Employee.BusinessLogic.Abstractions/IEmployeeManager.cs
@@ -6,11 +6,11 @@ namespace Employee.BusinessLogic.Abstractions
 {
     public interface IEmployeeManager
     {
-        IEnumerable<Model.Employee> GetAllEmployees(); // BUG: Should be ICollection instead of IEnumerable
+        ICollection<Model.Employee> GetAllEmployees(); // BUG: Should be ICollection instead of IEnumerable
 
         Model.Employee CreateEmployee(Model.Employee employee);
 
-        IEnumerable<Department> GetAllDepartments(); // BUG: Should be ICollection instead of IEnumerable
+        ICollection<Department> GetAllDepartments(); // BUG: Should be ICollection instead of IEnumerable
 
         void CreateDepartment(Department department);
     }
