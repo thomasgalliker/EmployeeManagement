8b3b62c113d759fd7ca04c43b498d5b0c72d0dd1
thomasgalliker <thomas@bluewin.ch>
2015-12-08 21:58:49 +0100

    Cleanup

--- Employee.WebApi/Controllers/HomeController.cs
+++ Employee.WebApi/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Employee.WebApi.Controllers
 {
@@ -10,9 +6,9 @@ namespace Employee.WebApi.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            this.ViewBag.Title = "Home Page";
 
-            return View();
+            return this.View();
         }
     }
 }
