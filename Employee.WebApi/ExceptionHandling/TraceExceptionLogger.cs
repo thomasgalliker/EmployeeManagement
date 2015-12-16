17f7dbda6315424ab1005d0ef14263aef06f9637
thomasgalliker <thomas@bluewin.ch>
2015-12-16 20:07:31 +0100

    Add missing files

---
+++ Employee.WebApi/ExceptionHandling/TraceExceptionLogger.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Web.Http.ExceptionHandling;
+
+namespace Employee.WebApi.ExceptionHandling
+{
+    public class TraceExceptionLogger : ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            Trace.TraceError(context.ExceptionContext.Exception.ToString());
+        }
+    }
+}
